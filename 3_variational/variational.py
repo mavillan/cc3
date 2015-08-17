eb93029b437af3dbbb2a623c0335dad36b663fac
mavillan <mavillan@alumnos.inf.utfsm.cl>
2015-08-17 01:39:27 -0300

    trying to implement multiprocessing to make it faster

--- 3_variational/variational.py
+++ 3_variational/variational.py
@@ -1,12 +1,32 @@
 import numpy as np 
-import sympy as sp
 import matplotlib.pyplot as plt
 from scipy import misc
 from scipy.integrate import odeint
 import pickle
-import cloudpickle
 import time
 
+""" Some constants """
+N=4 #image dimension
+
+
+""" Load "EL function" """
 fobj=open("sample.pickle","rb")
 F=pickle.load(fobj)
 fobj.close()
+
+
+
+# solve the system dc/dt = f(c, t)
+def f(c,t):
+	retval=[F[i](*c) for i in range(N**2)]
+	return retval
+
+
+# initial condition vector
+c0=np.random.rand(N**2)
+
+# time grid
+t  = np.linspace(0, 5., 1000)
+
+# solve that shit
+soln = odeint(f, c0, t)
--- 3_variational/variational_script++.py
+++ 3_variational/variational_script++.py
@@ -4,6 +4,7 @@ from scipy import misc
 import pickle
 import cloudpickle
 import time
+from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor
 
 t0=time.clock()
 
@@ -12,10 +13,10 @@ def lamb(func):
 
 """ Defining some functions """
 def psi1(x):
-	return np.square(x)
+	return x**2
 
 def psi2(x):
-	return np.square(x)
+	return x**2
 
 """ Taking discrete values of f function """
 fv=misc.imread('Circle-Missing-data-reduced-gray.png').astype(float)
@@ -23,9 +24,9 @@ fv=misc.imread('Circle-Missing-data-reduced-gray.png').astype(float)
 
 """ Some constants and parameter values """
 N=4 # Dimensions of image, assuming square form
-eps=np.float(0.5) #shape Parameters
-alpha=np.float(1.)
-beta=np.float(1.)	
+eps=sp.Float(0.5) #shape Parameters
+alpha=sp.Float(1.)
+beta=sp.Float(1.)	
 
 
 """ Defining Lagrangian funtion L(x,y,u,ux,uy,uxx,uxy,uyy) and calculating it's derivatives """
@@ -75,9 +76,9 @@ points=[]
 
 
 for i in range(N):
-	tmpx=xv[i]
+	tmpx=sp.Float(xv[i])
 	for j in range(N):
-		tmpy=yv[j]
+		tmpy=sp.Float(yv[j])
 		u+=c[N*i+j]*phi.subs(xi,tmpx).subs(yi,tmpy)
 		ux+=c[N*i+j]*phix.subs(xi,tmpx).subs(yi,tmpy)
 		uy+=c[N*i+j]*phiy.subs(xi,tmpx).subs(yi,tmpy)
@@ -93,6 +94,27 @@ Lux=Lux.subs(x2,ux).subs(x3,uy)
 Luy=Luy.subs(x2,ux).subs(x3,uy)
 Luxx=Luxx.subs(x4,uxx).subs(x5,uyy)
 Luyy=Luyy.subs(x4,uxx).subs(x5,uyy)
+
+def dx(expr):
+	return sp.diff(expr,x)
+
+def dy(expr):
+	return sp.diff(expr,y)
+
+def d2x(expr):
+	return sp.diff(expr,x,2)
+
+def d2y(expr):
+	return sp.diff(expr,y,2)
+
+result=[]
+
+with ProcessPoolExecutor(max_workers=4) as executor:
+	result.append(executor.submit(dx,Lux))
+	result.append(executor.submit(dy,Luy))
+	result.append(executor.submit(d2x,Luxx))
+	result.append(executor.submit(d2y,Luyy))
+
 EL=Lu-sp.diff(Lux,x)-sp.diff(Luy,y)+sp.diff(Luxx,x,2)+sp.diff(Luyy,y,2)
 
 
@@ -121,9 +143,3 @@ fobj.close()
 tf=time.clock()
 
 print("execution time:",tf-t0)
-
-
-
-
-
-
